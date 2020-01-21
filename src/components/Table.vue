1e88cbd8d372e0b5e9e5f51980c24d8bcf3548b5
Guilherme  Costa <guilhermecosta@MacBook-Air-de-Guilherme.local>
2020-01-21 11:24:27 +0000

    table

---
+++ src/components/Table.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <b-table
+      :bordered="bordered"
+      :hover="hover"
+      :items="items"
+      :fields="fields"
+    ></b-table>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        fields: ['Nome', 'Número'],
+        items: [
+          { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+          { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+          { age: 89, first_name: 'Geneva', last_name: 'Wilson' }
+        ],
+        bordered: true,
+        hover: true,
+      }
+    }
+  }
+</script>
