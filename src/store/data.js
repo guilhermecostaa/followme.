53f10f41c6865b8c5a50506178d60d3ebd73c45c
Guilherme  Costa <guilhermecosta@AirdeGuilherme.lan>
2020-02-07 14:49:52 +0000

    store changes

--- src/store/data.js
+++ src/store/data.js
@@ -1,6 +1,11 @@
 module.exports = {
     users: [
-        { id: 1, name: "Guilherme", number: "9170193", password: "1234", userType: "admin"},
-        { id: 2, name: "Gustavo", number: "9170196", password: "1234", userType: "user"}
+        { id: 1, name: "Guilherme", number: "9170193", password: "1234", userType: "admin" },
+        { id: 2, name: "Gustavo", number: "9170196", password: "1234", userType: "user" }
+    ],
+    interestPoints: [
+        { id: 1, name: "Torre dos Clérigos", city: "Porto", desc: "Vista da cidade em torre barroca" },
+        { id: 2, name: "Livraria Lello", city: "Porto", desc: "Livraria ornada com escadaria vermelha" },
+        { id: 3, name: "Ponte D.Luís", city: "Porto", desc: "Ponte icónica de metal sobre o Douro" }
     ]
 }
--- src/store/index.js
+++ src/store/index.js
@@ -8,11 +8,7 @@ export default new Vuex.Store({
     users: [],
     loggedUserId: 0,
     points: [],
-    /*interestPoints: [
-      {id: 1, name:"Torre dos Clérigos", city:"Porto", desc:"Vista da cidade em torre barroca"},
-      {id: 2, name:"Livraria Lello", city:"Porto", desc:"Livraria ornada com escadaria vermelha"},
-      {id: 3, name:"Ponte D.Luís", city:"Porto", desc:"Ponte icónica de metal sobre o Douro"}
-    ]*/
+    interestPoints: []
   },
   mutations: {
     USER_LOGGED_IN(state,id){
@@ -80,6 +76,6 @@ export default new Vuex.Store({
         }
       }
       return type
-    }
+    },
   }
 })
